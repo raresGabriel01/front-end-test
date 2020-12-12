f959940a4100af87309f9b2123f8610b77ebe788
raresGabriel01 <baesu_r@yahoo.com>
2020-12-12 20:09:16 +0200

    added some final details

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -4,7 +4,7 @@ import './Grid.css'
 import Card from './Card.js'
 import PopUp from './PopUp.js'
 function Grid() {
-	const [data, setData] = useState([])
+	const [data, setData] = useState(null)
 	const [popUp, setPopUp] = useState(false)
 	const [index, setIndex] = useState(null)
 	useEffect(() => {
@@ -17,16 +17,20 @@ function Grid() {
 	},[])
 
 	
-
-	return(
-		<div>
-			{popUp ? <PopUp data = {data[index-1]} closePopUp = {closePopUp}/> : ""}
-			<div className = "grid">
-				{data.map(x => <Card key = {x.id} data = {x} showPopUp ={showPopUp} />)}
+	if(data)
+		return(
+		 
+			<div>
+				{popUp ? <PopUp data = {data[index-1]} closePopUp = {closePopUp}/> : ""}
+				<div className = "grid">
+					{data.map(x => <Card key = {x.id} data = {x} showPopUp ={showPopUp} />)}
+				</div>
 			</div>
-		</div>
+		)
+	else {
+		return(<h1> Wait for the page to load ... </h1>)
+	}
 
-	)
 
 	function showPopUp(index) {
 		setIndex(index)
